87ac43d0dc7bee979e2fead75ae7652041df93ab
AlastarXIII <119960374+AlastarXIII@users.noreply.github.com>
2023-11-07 21:52:41 +0100

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -60,9 +60,6 @@ Set-WallPaper -Image "C:\image.png" -Style Center
 Invoke-WebRequest -Uri "https://docs.google.com/uc?export=download&id=1PbQQ4p2jAoFUQWZRDgsR-PPBgxYxuqsZ" -OutFile "$dir\image.png"
 Set-WallPaper -Image "$dir\image.png" -Style Fit
 
-Invoke-WebRequest -Uri "https://docs.google.com/uc?export=download&id=1PbQQ4p2jAoFUQWZRDgsR-PPBgxYxuqsZ" -OutFile "$dir\image.png"
-Set-WallPaper -Image "$dir\image.png" -Style Fit
-
 Add-Type -AssemblyName presentationCore
 $filepath = "$dir\video.mp4"
 
